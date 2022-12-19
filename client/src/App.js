5cca8edbf02954e889de8fe7406513e98cc3a70c
Daniela Lopez <dlope0831@gmail.com>
2022-12-18 18:48:15 -0600

    added develop branch changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import {
   ApolloClient,
   InMemoryCache,
@@ -8,12 +8,13 @@ import {
 } from "@apollo/client"
 import { setContext } from "@apollo/client/link/context"
 import "semantic-ui-css/semantic.min.css"
-
+import Sidebar from './components/Navbar'
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
-// import { StoreProvider } from "./utils/GlobalState";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Recipes from "./pages/Recipes"
+import Saved from './pages/Savedrecipes'
 
 const httpLink = createHttpLink({
   uri: "/graphql",
@@ -38,11 +39,14 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
+    <Sidebar/>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/savedrecipes" element={<Saved />} />
           </Routes>
           </div>
       </Router>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,93 @@
-import React from "react";
+import React from "react"
+import asian from "../assets/asian.jpg"
+import caribbean from "../assets/caribbean.jpg"
+import indian from "../assets/indian.jpg"
+import italian from "../assets/italian.jpg"
+import spanish from "../assets/spanish.jpg"
+import { Header, Image, Grid } from "semantic-ui-react"
 
-function Home (){
-return (
+function Home() {
+  return (
     <div>
-        Home
-        <br/>
-        <a href="/login">Login</a>
-        <br/>
-        <a href="/signup">signup</a>
+      <Header as="h2" inverted textAlign="center" color= "blue">
+        Home <br/>
+      </Header>
+      <Grid text style={{ marginTop: "7em" }}>
+        <Grid.Row columns={5}>
+          <Grid.Column>
+            <Image
+              label={{
+                size: "medium",
+                color: "blue",
+                content: "Spanish",
+                icon: "utensils",
+                ribbon: true,
+              }}
+              src={spanish}
+              size="medium"
+              rounded
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              label={{
+                size: "medium",
+                color: "green",
+                content: "Italian ",
+                icon: "utensils",
+                ribbon: true,
+              }}
+              src={italian}
+              size="medium"
+              rounded
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              label={{
+                size: "medium",
+                color: "orange",
+                content: "Indian",
+                icon: "utensils",
+                ribbon: true,
+              }}
+              src={indian}
+              size="medium"
+              rounded
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              label={{
+                size: "medium",
+                color: "red",
+                content: "East Asian",
+                icon: "utensils",
+                ribbon: true,
+              }}
+              src={asian}
+              size="medium"
+              rounded
+            />
+          </Grid.Column>
+          <Grid.Column>
+            <Image
+              label={{
+                size: "medium",
+                color: "yellow",
+                content: "Caribbean",
+                icon: "utensils",
+                ribbon: true,
+              }}
+              src={caribbean}
+              size="medium"
+              rounded
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
-)
+  )
 }
 
-export default Home;
+export default Home
