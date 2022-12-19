bf1eee692637271457c7b34d3e276ee290c3d694
Shanice Sauceda <shanicesauceda@gmail.com>
2022-12-19 16:59:17 -0600

    search bar searches

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
 import Computer from "../components/Home/Comp"
 import Tablet from "../components/Home/Tablet"
 import Mobile from "../components/Home/Mobile"
-import { Grid, Search } from "semantic-ui-react"
+import { Grid, Input } from "semantic-ui-react"
 
 function Home() {
+  const [searchState, setSearchState] = useState("")
+
+  const handleSearch = (e) => {
+    const searchInput = e.target.value
+
+    setSearchState(searchInput)
+  }
+
+  const handleClick = () => {
+    console.log(searchState)
+  }
+
   return (
     <>
       <Computer />
-        <Grid textAlign="center">
-          <Grid.Row stretched>
-            <Grid.Column>
-              <Search
-                input={{ icon: "search", iconPosition: "left" }}
-                size="large"
-                placeholder="Search for a recipe"
-
-              />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+      <Grid textAlign="center">
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Input
+              size="large"
+              placeholder="Search for a recipe"
+              action={{
+                icon: "search",
+                onClick: () => {
+                  handleClick()
+                },
+              }}
+                onChange={(e) => {
+              handleSearch(e)
+                }}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
       <Mobile />
       <Tablet />
-
     </>
   )
 }
