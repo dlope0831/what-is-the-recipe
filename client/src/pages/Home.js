c680935be2be90afb14101d43c9649e89df47294
Daniela Lopez <dlope0831@gmail.com>
2022-12-19 15:36:45 -0600

    trying to fix merge conflicts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,11 @@
 import React from "react"
-<<<<<<<<< Temporary merge branch 1
-import Computer from "../components/Home/Comp"
-import Tablet from "../components/Home/Tablet"
-import Mobile from "../components/Home/Mobile"
-import { Header, Grid, Search } from "semantic-ui-react"
-=========
 import asian from "../assets/asian.jpg"
 import caribbean from "../assets/caribbean.jpg"
 import indian from "../assets/indian.jpg"
 import italian from "../assets/italian.jpg"
 import spanish from "../assets/spanish.jpg"
 import { Header, Image, Grid } from "semantic-ui-react"
->>>>>>>>> Temporary merge branch 2
+
 
 function Home() {
   return (
