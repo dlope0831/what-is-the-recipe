ebbd054dc0f8d8de660e14d1c915c214a6978944
jeni <jenireecastro@gmail.com>
2022-12-18 18:29:30 -0600

    changed navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ import {Outlet, Link } from "react-router-dom"
 const Bar = () => {
   return (
     <>  
-    <Navbar bg="light" expand="lg" >
+    <Navbar bg="dark" variant="dark" expand="lg" >
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -17,7 +17,7 @@ const Bar = () => {
             <Nav.Link as={Link} to="/recipes">Recipes</Nav.Link>
             <Nav.Link as={Link} to="/savedrecipes">Saved</Nav.Link>
             </Nav>
-            <Button variant="outline-success" href="/login">Login</Button>
+            <Button variant="outline-light" href="/login">Login</Button>
             <Button className="m-2" href="/signup">Sign Up</Button>
         </Navbar.Collapse>
       </Container>
