68207d6de4c7cc4e6837bd99b4207ed75b6bc00b
Daniela Lopez <dlope0831@gmail.com>
2022-12-21 17:02:17 -0600

    add minor changes to searchrecipe

--- client/src/pages/SearchRecipes.js
+++ client/src/pages/SearchRecipes.js
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from "react"
-import { Embed, Button } from "semantic-ui-react"
+import { Grid, Input, Embed } from "semantic-ui-react"
 
-import { Jumbotron, Container, Col, Form } from "react-bootstrap"
+// import {
+//   Jumbotron,
+//   Container,
+//   Col,
+//   Form,
+//   //   Button,
+//   //   Card,
+//   //   CardColumns,
+// } from "react-bootstrap"
 
-import Auth from "../utils/auth"
+// import Auth from '../utils/auth';
 import { searchYoutubeShorts } from "../utils/API"
-import { saveRecipeIds, getSavedRecipeIds } from "../utils/localStorage"
-import { useMutation } from "@apollo/client"
-import { SAVE_RECIPE } from "../utils/mutations"
+// import { saveRecipeIds, getSavedRecipeIds } from '../utils/localStorage';
+// import { useMutation } from '@apollo/client';
+// import { SAVE_RECIPE } from '../utils/mutations';
 
-const SearchRecipes = () => {
+function SearchRecipes() {
   const [recipeData, setRecipeData] = useState([])
 
   const [inputVal, setInputVal] = useState("")
 
-  const [query, setQuery] = useState("ramen")
+  const [query, setQuery] = useState("foodie")
 
-  const [savedRecipeIds, setSavedRecipeIds] = useState(getSavedRecipeIds())
+  //   const [savedRecipeIds, setSavedRecipeIds] = useState(getSavedRecipeIds());
 
-  const [saveRecipe] = useMutation(SAVE_RECIPE)
-
-  useEffect(() => {
-    return () => saveRecipeIds(savedRecipeIds)
-  })
+  //   const [saveRecipe, { error }] = useMutation(SAVE_RECIPE);
 
   const handleUpdate = (e) => {
     setInputVal(e.target.value)
@@ -62,279 +66,100 @@ const SearchRecipes = () => {
       const { items } = await response.json()
 
       const recipeData = items.map((short) => ({
-        recipeId: short.id.videoId,
         title: short.snippet.title,
         description: short.snippet.description,
       }))
 
       setQuery(recipeData)
       setInputVal("")
-      console.log(recipeData)
     } catch (err) {
       console.error(err)
     }
   }
 
-  const handleSaveRecipe = async (recipeId) => {
-    console.log(recipeId) // log the recipeId here
+  //   const handleSaveRecipe = async (recipeId) => {
 
-    const recipeToSave = recipeData.find(
-      (recipe) => recipe.recipeId === recipeId
-    )
+  //     const recipeToSave = searchedRecipes.find((recipe) => recipe.recipeId === recipeId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null
+  //     // get token
+  //     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false
-    }
+  //     if (!token) {
+  //       return false;
+  //     }
 
-    try {
-      await saveRecipe({
-        variables: { recipe: { ...recipeToSave } },
-      })
+  //     try {
 
-      setSavedRecipeIds([...savedRecipeIds, recipeToSave.recipeId])
-    } catch (err) {
-      console.error(err)
-    }
-    console.log(savedRecipeIds)
-  }
+  //       await saveRecipe({
+  //         variables: { recipe: { ...recipeToSave } },
+  //       });
+
+  //       setSavedRecipeIds([...savedRecipeIds, recipeToSave.recipeId]);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
 
   return (
     <>
-      <Jumbotron fluid className="text-light bg-dark">
-        <Container>
-          <h1>Search for Recipes!</h1>
-          <Form onSubmit={handleFormSubmit}>
-            <Col xs={12} md={8}>
-              <Form.Control
-                name="inputVal"
-                value={inputVal}
-                onChange={handleUpdate}
-                type="text"
-                size="lg"
-                placeholder="Search for a recipe"
-              />
-            </Col>
-            <Col xs={12} md={4}>
-              <div>
-                {/* <p>{inputVal} </p>
-            <input type="text" value={inputVal} onChange={handleUpdate} /> */}
-                <button onClick={handleSearch}>Search</button>
-              </div>
-            </Col>
-          </Form>
-        </Container>
-      </Jumbotron>
-      <Container>
-        <div>
-          {recipeData.map((short, i) => {
-            return (
-              <div key={i}>
-                Title: {short.snippet.title}
-                <br />
-                Description: {short.snippet.description}
-                <br />
-                {short.snippet.description}
-                <Embed
-                  id={short.id.videoId}
-                  source="youtube"
-                  // url= {`www.youtube.com/shorts/${query}`}
+      <Grid textAlign="center">
+        <Grid.Row stretched>
+          <Grid.Column>
+            <Input
+              size="large"
+              name="inputVal"
+              value={inputVal}
+              placeholder="Search for a recipe"
+              action={{
+                icon: "search",
+                onClick: () => {
+                  handleSearch()
+                },
+              }}
+              onChange={(e) => {
+                handleUpdate(e)
+              }}
+              onSubmit={handleFormSubmit}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
-                  iframe={{
-                    allowFullScreen: true,
-                  }}
-                  aspectRatio="4:3"
-                />
-                {Auth.loggedIn() && (
-                    <Button
-                      disabled={savedRecipeIds?.some((savedRecipeId) => savedRecipeId === short.recipeId)}
-                      className='btn-block btn-info'
-                      onClick={() => handleSaveRecipe(short.recipeId)}>
-                      {savedRecipeIds?.some((savedRecipeId) => savedRecipeId === short.recipeId)
-                        ? 'This recipe has already been saved!'
-                        : 'Save this Recipe!'}
-                    </Button>
-                  )}
-                {/* <button onClick={() => handleSaveRecipe(short.id.videoId)}>
-                  Save Recipe
-                </button> */}
-              </div>
-            )
-          })}
-        </div>
-      </Container>
-    </>
-  )
-}
+      <Grid
+        style={{ padding: "55px" }}
+        columns={2}
+        className="topVids"
+        stackable
+        textAlign="center"
+      >
+        <Grid.Row>
+          <Grid.Column>
+            {recipeData.map((short, i) => {
+              return (
+                <div key={i}>
+                  Title:
+                  <br />
+                  {short.snippet.title}
+                  <br />
+                  Description:
+                  <br />
+                  {short.snippet.description}
+                  <Embed
+                    id={short.id.videoId}
+                    source="youtube"
+                    iframe={{
+                      allowFullScreen: true,
+                    }}
+                    aspectRatio="4:3"
+                  />
+                </div>
+              )
+            })}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
 
-export default SearchRecipes
-
-// import React, { useState, useEffect } from "react"
-// import { Embed } from "semantic-ui-react"
-
-// import {
-//   Jumbotron,
-//   Container,
-//   Col,
-//   Form
-// } from "react-bootstrap"
-
-// import Auth from '../utils/auth';
-// import { searchYoutubeShorts } from "../utils/API"
-// import { saveRecipeIds, getSavedRecipeIds } from "../utils/localStorage"
-// import { useMutation } from "@apollo/client"
-// import { SAVE_RECIPE } from "../utils/mutations"
-
-// const SearchRecipes = () => {
-//   const [recipeData, setRecipeData] = useState([])
-
-//   const [inputVal, setInputVal] = useState("")
-
-//   const [query, setQuery] = useState("ramen")
-
-//   const [savedRecipeIds, setSavedRecipeIds] = useState(getSavedRecipeIds())
-
-//   const [saveRecipe] = useMutation(SAVE_RECIPE)
-
-//   useEffect(() => {
-//     return () => saveRecipeIds(savedRecipeIds)
-//   })
-
-//   const handleUpdate = (e) => {
-//     setInputVal(e.target.value)
-//   }
-
-//   const handleSearch = () => {
-//     console.log(inputVal)
-//     setQuery(inputVal)
-//   }
-
-//   useEffect(() => {
-//     fetch(`/api/shorts/${query}`)
-//       .then((resp) => {
-//         return resp.json()
-//       })
-//       .then((ytData) => {
-//         console.log(typeof ytData)
-//         console.log(ytData)
-//         setRecipeData(ytData)
-//       })
-//   }, [query])
-
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault()
-
-//     if (!inputVal) {
-//       return false
-//     }
-
-//     try {
-//       const response = await searchYoutubeShorts(inputVal)
-
-//       if (!response.ok) {
-//         throw new Error("something went wrong!")
-//       }
-
-//       const { items } = await response.json()
-
-//       const recipeData = items.map((short) => ({
-//         recipeId: short.id,
-//         title: short.snippet.title,
-//         description: short.snippet.description,
-//       }))
-
-//       setQuery(recipeData)
-//       setInputVal("")
-//       console.log(recipeData)
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }
-
-//   const handleSaveRecipe = async (recipeId) => {
-//     const recipeToSave = searchedRecipes.find(
-//       (recipe) => recipe.recipeId === recipeId
-//     )
-
-//     // get token
-//     const token = Auth.loggedIn() ? Auth.getToken() : null
-
-//     if (!token) {
-//       return false
-//     }
-
-//     try {
-//       await saveRecipe({
-//         variables: { recipe: { ...recipeToSave } },
-//       })
-
-//       setSavedRecipeIds([...savedRecipeIds, recipeToSave.recipeId])
-//     } catch (err) {
-//       console.error(err)
-//     }
-//   }
-
-//   return (
-//     <>
-//       <Jumbotron fluid className="text-light bg-dark">
-//         <Container>
-//           <h1>Search for Recipes!</h1>
-//           <Form onSubmit={handleFormSubmit}>
-//             <Col xs={12} md={8}>
-//               <Form.Control
-//                 name="inputVal"
-//                 value={inputVal}
-//                 onChange={handleUpdate}
-//                 type="text"
-//                 size="lg"
-//                 placeholder="Search for a recipe"
-//               />
-//             </Col>
-//             <Col xs={12} md={4}>
-//               <div>
-//                 {/* <p>{inputVal} </p>
-//             <input type="text" value={inputVal} onChange={handleUpdate} /> */}
-//                 <button onClick={handleSearch}>Search</button>
-
-//                 {recipeData.map((short, i) => {
-//                   return (
-//                     <div key={i}>
-//                       Title:
-//                       <br></br>
-//                       {short.snippet.title}
-//                       <br></br>
-//                       Description:
-//                       <br></br>
-//                       {short.snippet.description}
-//                       <Embed
-//                         id={short.id.videoId}
-//                         source="youtube"
-//                         // url= {`www.youtube.com/shorts/${query}`}
-
-//                         iframe={{
-//                           allowFullScreen: true,
-//                         }}
-//                         aspectRatio="4:3"
-//                       />
-//                       {console.log(short.snippet.videoId)}
-//                     </div>
-//                   )
-//                 })}
-//               </div>
-//             </Col>
-//           </Form>
-//         </Container>
-//       </Jumbotron>
-//     </>
-//   )
-// }
-
-// export default SearchRecipes
-
-// {
-/* <Container>
+      {/* <Container>
         <h2>
           {recipeData.length
             ? `Viewing ${recipeData.length} results:`
@@ -376,9 +201,9 @@ export default SearchRecipes
             );
           })}
         </CardColumns>
-      </Container> */
-// }
+      </Container> */}
+    </>
+  )
+}
 
-//   Button,
-//   Card,
-//   CardColumns,
+export default SearchRecipes
