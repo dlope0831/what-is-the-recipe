e2f92d9518effb2882d3c42bc50bd34fc2c994f8
Shanice Sauceda <shanicesauceda@gmail.com>
2022-12-21 12:23:39 -0600

    removed psuedocode

--- client/src/pages/SearchRecipes.js
+++ client/src/pages/SearchRecipes.js
@@ -1,21 +1,8 @@
 import React, { useState, useEffect } from "react"
 import { Grid, Input, Embed } from "semantic-ui-react"
 
-// import {
-//   Jumbotron,
-//   Container,
-//   Col,
-//   Form,
-//   //   Button,
-//   //   Card,
-//   //   CardColumns,
-// } from "react-bootstrap"
-
-// import Auth from '../utils/auth';
 import { searchYoutubeShorts } from "../utils/API"
-// import { saveRecipeIds, getSavedRecipeIds } from '../utils/localStorage';
-// import { useMutation } from '@apollo/client';
-// import { SAVE_RECIPE } from '../utils/mutations';
+
 
 function SearchRecipes() {
   const [recipeData, setRecipeData] = useState([])
@@ -23,10 +10,6 @@ function SearchRecipes() {
   const [inputVal, setInputVal] = useState("")
 
   const [query, setQuery] = useState("foodie")
-
-  //   const [savedRecipeIds, setSavedRecipeIds] = useState(getSavedRecipeIds());
-
-  //   const [saveRecipe, { error }] = useMutation(SAVE_RECIPE);
 
   const handleUpdate = (e) => {
     setInputVal(e.target.value)
@@ -77,28 +60,7 @@ function SearchRecipes() {
     }
   }
 
-  //   const handleSaveRecipe = async (recipeId) => {
-
-  //     const recipeToSave = searchedRecipes.find((recipe) => recipe.recipeId === recipeId);
-
-  //     // get token
-  //     const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //     if (!token) {
-  //       return false;
-  //     }
-
-  //     try {
-
-  //       await saveRecipe({
-  //         variables: { recipe: { ...recipeToSave } },
-  //       });
-
-  //       setSavedRecipeIds([...savedRecipeIds, recipeToSave.recipeId]);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
+  
 
   return (
     <>
@@ -158,50 +120,6 @@ function SearchRecipes() {
           </Grid.Column>
         </Grid.Row>
       </Grid>
-
-      {/* <Container>
-        <h2>
-          {recipeData.length
-            ? `Viewing ${recipeData.length} results:`
-            : 'Search for a recipe to begin'}
-        </h2>
-        <CardColumns>
-          {recipeData.map((short, i) => {
-            return (
-              <Card key={recipe.recipeId} border="dark">
-                {recipe.image ? (
-                  <Card.Img
-                    src={recipe.image}
-                    alt={`The cover for ${recipe.title}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{recipe.title}</Card.Title>
-                  <p className="small">Authors: {recipe.authors}</p>
-                  <Card.Text>{recipe.description}</Card.Text>
-                  {Auth.loggedIn() && (
-                    <Button
-                      disabled={savedRecipeIds?.some(
-                        (savedRecipeId) => savedRecipeId === recipe.recipeId
-                      )}
-                      className="btn-block btn-info"
-                      onClick={() => handleSaveRecipe(recipe.recipeId)}
-                    >
-                      {savedRecipeIds?.some(
-                        (savedRecipeId) => savedRecipeId === recipe.recipeId
-                      )
-                        ? 'This recipe has already been saved!'
-                        : 'Save this Recipe!'}
-                    </Button>
-                  )}
-                  {error && <div>Something went wrong!</div>}
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </CardColumns>
-      </Container> */}
     </>
   )
 }
