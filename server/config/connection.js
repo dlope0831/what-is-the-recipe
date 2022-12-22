c5939553c77ab9de79393c7e8633488f5c1c385e
Daniela Lopez <dlope0831@gmail.com>
2022-12-21 20:55:18 -0600

    deleted proxy

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1/what-is-the-recipe',
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/what-is-the-recipe',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
