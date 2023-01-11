1174f60e12bd9262ae50cc14e65f0e64010d7a65
Daniela Lopez <dlope0831@gmail.com>
2023-01-11 17:49:40 -0600

    added the * to troubleshoot

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require("./schemas");
@@ -27,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // app.use(routes);
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
 app.get('/api/shorts/:q', (req, res) => {
